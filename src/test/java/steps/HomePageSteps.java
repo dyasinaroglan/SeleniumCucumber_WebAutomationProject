cb385131e30070deb19893bb33548e57fca0a82b
AR009 <yasinargln264@gmail.com>
2026-02-22 21:17:15 +0300

    Fix tab switching with window handles, first passing test

--- src/test/java/steps/HomePageSteps.java
+++ src/test/java/steps/HomePageSteps.java
@@ -9,9 +9,13 @@ import org.openqa.selenium.By;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Set;
+
 public class HomePageSteps {
 
     private static final Logger log = LoggerFactory.getLogger(HomePageSteps.class);
+
+    String tempData;
 
     @Given("{string} hesapkurdu ana sayfasına gidilir")
     public void goToSite(String url) {
@@ -23,16 +27,28 @@ public class HomePageSteps {
 
     @When("ihtiyaç kredisi butonuna tıklanır")
     public void ihtiyaçKredisiButonunaTıklanır() {
+
+        tempData = DriverManager.driver.getWindowHandle();
         DriverManager.driver.findElement(By.xpath("//div[@class='Container_innerContainer__nz5R5 Hero_features__IiWyG relative']//a[contains(text(), 'İhtiyaç Kredisi') and @href='/ihtiyac-kredisi']")).click();
     }
 
     @Then("İhtiyaç kredisi gidildiği kontrol edilir")
-    public void i̇htiyaçKredisiGidildiğiKontrolEdilir() {
+    public void i̇htiyaçKredisiGidildiğiKontrolEdilir() throws InterruptedException {
+
+        Thread.sleep(2000);
+        Set<String> handles = DriverManager.driver.getWindowHandles();
+        for (String handle : handles) {
+            if (!handle.equals(tempData)) {
+                DriverManager.driver.switchTo().window(handle);
+            }
+        }
         String actualUrl = DriverManager.driver.getCurrentUrl();
-        Assert.assertEquals("https://www.hesapkurdu.com/ihtiyac-kredisi", actualUrl);
+        Assert.assertEquals("https://www.hesapkurdu.com/ihtiyac-kredisi",actualUrl);
+        log.info(actualUrl + " sayfasına gidildi.");
     }
+
     @Then("Tarayıcı kapatılır.")
-    public void tarayiciKapatlir(){
+    public void tarayiciKapatlir() {
         DriverManager.quitDriver();
     }
 }
